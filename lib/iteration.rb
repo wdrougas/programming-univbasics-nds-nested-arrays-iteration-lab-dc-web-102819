ec5be271b5de785179622b29806f3bf6fabe9a75
Will Drougas <wdrougas@gmail.com>
2019-10-15 20:49:09 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -1,9 +1,11 @@
 def join_ingredients(src)
   new_array = []
   i = 0
-  # while i < src.length do 
-    
-  # end
+  while i < src.length do 
+    new_array.push("I love #{src[i][0]} and #{src[i][1]} on my pizza")
+    i += 1
+  end
+  new_array
 end
 
 def find_greater_pair(src)
